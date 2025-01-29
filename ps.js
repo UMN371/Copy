eb612e47bd9df52f43e14db4564b4c485d13e630
luxxx371 <luxxx371@umn.edu>
2025-01-29 14:26:15 -0600

    ps.js

---
+++ ps.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const password = prompt('Enter the password to access this page:');
+
+    // Replace 'yourpassword' with the actual password you want to use
+    if (password === 'Momcology2025') {
+        document.getElementById('protected-content').style.display = 'block';
+    } else {
+        alert('Incorrect password!');
+        window.location.href = 'https://momcology.org/'; // Redirect to another page if the password is incorrect
+    }
+});
